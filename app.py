63b10ec70df4f8a417adbf467ebb9df9555d4f8e
Anant-Paliwal <anant31122000@gmail.com>
2023-02-09 20:48:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-import tensorflow.compat.v2 as tf
 import streamlit as st
 import tensorflow as tf
 from keras.models import load_model
